be0655c245b75b24548a3f31b7da04110ba5e2f9
jiangtao5 <jiangtao5@asiainfo.com>
2019-06-20 21:52:20 +0800

    跨域处理{header: {'Origin': '*'} 对应后台 获取  this.$http.post('http://localhost:8080/blog-admin/login', {username: name, password: pwd}, {header: {'Origin': '*'}, emulateJSON: true})

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,141 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+       <el-container>
+         <el-header>
+           <el-menu
+             :default-active="activeIndex2"
+             class="el-menu-demo"
+             mode="horizontal"
+             @select="handleSelect"
+             background-color="#545c64"
+             text-color="#fff"
+             active-text-color="#ffd04b">
+             <el-menu-item index="1">处理中心</el-menu-item>
+             <el-submenu index="2">
+               <template slot="title">我的工作台</template>
+               <el-menu-item index="2-1">选项1</el-menu-item>
+               <el-menu-item index="2-2">选项2</el-menu-item>
+               <el-menu-item index="2-3">选项3</el-menu-item>
+               <el-submenu index="2-4">
+                 <template slot="title">选项4</template>
+                 <el-menu-item index="2-4-1">选项1</el-menu-item>
+                 <el-menu-item index="2-4-2">选项2</el-menu-item>
+                 <el-menu-item index="2-4-3">选项3</el-menu-item>
+               </el-submenu>
+             </el-submenu>
+             <el-menu-item index="3" disabled>消息中心</el-menu-item>
+             <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+             <el-button type="primary" id="loginLbl" @click="loginLink">登录</el-button>
+
+             <el-dialog title="登录" :visible.sync="dialogFormVisible" top="180px">
+               <el-form :model="form" ref="loginForm">
+                 <el-form-item label="姓名:" :label-width="formLabelWidth">
+                   <el-input v-model="form.name" autocomplete="off"></el-input>
+                   <span v-if="form.error.name" class="err-msg">{{form.error.name}}</span>
+                 </el-form-item>
+                 <el-form-item label="密码:" :label-width="formLabelWidth">
+                   <el-input v-model="form.pwd" autocomplete="off"></el-input>
+                   <span v-if="form.error.pwd" class="err-msg">{{form.error.pwd}}</span>
+                 </el-form-item>
+               </el-form>
+               <div  >
+                 <el-button @click="dialogFormVisible = false">取 消</el-button>
+                 <el-button type="primary" @click="loginSubmit('loginForm')">登 录</el-button>
+               </div>
+             </el-dialog>
+           </el-menu>
+         </el-header>
+         <el-main>
+           <router-view/>
+         </el-main>
+         <el-footer></el-footer>
+       </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1',
+      dialogFormVisible: false,
+      form: {
+        name: '',
+        pwd: '',
+        error: {
+          name: '',
+          pwd: ''
+        }
+      },
+      formLabelWidth: '60px'
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    loginLink () {
+      this.dialogFormVisible = true
+    },
+    check (name, pwd) {
+      console.log(this.form)
+      if (!name) {
+        this.form.error.name = '请输入姓名'
+        return false
+      }
+      if (!pwd) {
+        this.form.error.pwd = '请输入密码'
+        return false
+      }
+      return true
+    },
+    loginSubmit (loginForm) {
+      const name = this.form.name
+      const pwd = this.form.pwd
+      const $router = this.$router
+      console.log($router)
+      if (!this.check(name, pwd)) return
+      if (name === 'root' && pwd === '278495617') {
+        alert('name:' + name + 'pwd:' + pwd)
+        this.$http.post('http://localhost:8080/blog-admin/login', {username: name, password: pwd}, {header: {'Origin': '*'}, emulateJSON: true})
+          .then(
+            (response) => {
+              console.log(response.headers)
+              console.log(response)
+            },
+            (error) => {
+              alert(error)
+              console.log(error)
+            }
+          )
+        this.dialogFormVisible = false
+        $router.push('/hello')
+      } else {
+        alert('用户名密码错误')
+      }
+    }
+
+  }
 }
 </script>
 
 <style>
-#app {
+ #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
 }
+
+  #loginLbl{
+    display:block;
+    margin-left: auto;
+    margin-right: 5%;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Demo from '@/components/demo'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'demo',
       component: Demo
+    },
+    {
+      path: '/hello',
+      name: 'hello',
+      component: HelloWorld
     }
   ]
 })
