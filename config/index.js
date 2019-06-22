b256f76ae8a6ef6fdf3eae8bb10fb2f650f70cc2
jiangtao5 <jiangtao5@asiainfo.com>
2019-06-22 14:56:44 +0800

    代理设置 首先在config/index.js中配置proxyTable 如果接口跨域，需要进行这个参数配置 proxyTable: {       '/api':{           // target:'http://jsonplaceholder.typicode.com',           target:'http://localhost:9080',           changeOrigin:true,
               pathRewrite:{
                   '/api':''
               }
          }

--- config/index.js
+++ config/index.js
@@ -10,7 +10,17 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
-    proxyTable: {},
+    //http://localhost:8080/blog-web/login
+    proxyTable: {
+      '/blog-web': {
+        target: 'http://localhost:8080', // 接口的域名
+        // secure: false,  // 如果是https接口，需要配置这个参数
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+        pathRewrite: {
+          '/blog-web': ''
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
